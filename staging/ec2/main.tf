9272a04ff85d3a23fe2537e83681eaae515ecb0e
mirdan.syahid@dana.id <mirdan.syahid@dana.id>
2024-04-24 21:59:44 +0700

    change instance type to micro

--- staging/ec2/main.tf
+++ staging/ec2/main.tf
@@ -2,7 +2,7 @@ module "web-server" {
   source = "../../modules/ec2"
   
   ami                    = "ami-015f72d56355ebc27"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   tag_name               = "web-server"
   instance_min_count     = 3
   instance_max_count     = 4
