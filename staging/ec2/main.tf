22cf34152badacb4a56f0f377f27e262f77ef1c4
mirdan.syahid@dana.id <mirdan.syahid@dana.id>
2024-04-24 22:30:32 +0700

    Change instance count to 3

--- staging/ec2/main.tf
+++ staging/ec2/main.tf
@@ -4,7 +4,7 @@ module "web-server" {
   ami                    = "ami-015f72d56355ebc27"
   instance_type          = "t2.micro"
   tag_name               = "web-server"
-  instance_min_count     = 2
+  instance_min_count     = 3
   instance_max_count     = 4
   cluster_name           = "web-server"
   key_pair               = "terraform-key-pair"
